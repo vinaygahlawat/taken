e4d638ea7eb5187c484fb380ec6a9403260b3035
Vinay Gahlawat <vinay.gahlawat@gmail.com>
2024-04-15 11:51:35 -0500

    Add initial Player class and unit tests.

--- source/player.py
+++ source/player.py
@@ -1,2 +1,29 @@
+from source.card import Card
+from source.dealer import Dealer
+
+'''
+Player object needs to represent either a human or computer player.
+Needs to be able to accept a hand of Card objects dealt by the Dealer.
+Needs to be able to select a card to play based on the board conditions.
+This card needs to be removed from the hand and sent to the dealer.
+'''
 class Player:
-    pass
+
+    def __init__(self, dealer, is_computer: bool = True) -> None:
+        self.dealer = dealer
+        self.is_computer = is_computer
+        self.hand = []
+
+    @property
+    def hand(self):
+        return self._hand
+
+    @hand.setter
+    def hand(self, value):
+        self._hand = value
+
+    def play_card(self) -> None:
+        self.dealer.play_card(self, self.choose_card())
+
+    def choose_card(self) -> Card:
+        return self.hand[0]
---
+++ tests/test_player.py
@@ -0,0 +1,29 @@
+import unittest
+from source.player import Player
+from source.card import Card
+
+class TestPlayer(unittest.TestCase):
+
+    def test_player_init(self):
+        player = Player(None)
+        self.assertListEqual(player.hand, [])
+
+    def test_player_set_hand(self):
+        player = Player(None)
+        test_hand = [Card(1, 1), Card(7,3)]
+        player.hand = test_hand
+        self.assertListEqual(player.hand, test_hand)
+        self.assertEqual(player.hand[0].number, 1)
+        self.assertEqual(player.hand[0].number, test_hand[0].number)
+        self.assertEqual(player.hand[1].points, 3)
+        self.assertEqual(player.hand[1].points, test_hand[1].points)
+
+    def test_choose_card(self):
+        player = Player(None)
+        player.hand = [Card(2, 7), Card(7,3)]
+        self.assertEqual(player.choose_card().number, 2)
+        self.assertEqual(player.choose_card().points, 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
