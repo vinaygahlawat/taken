8f40cc2fbd92787a99e24c500c0c9b2141dd7e16
Vinay Gahlawat <vinay.gahlawat@gmail.com>
2024-04-15 16:54:30 -0500

    Move CardDeck collection from class variable to instance variable.

--- source/deck.py
+++ source/deck.py
@@ -2,11 +2,9 @@ from source.card import Card
 
 class Deck:
 
-    # Static class attributes
-    CardDeck = []
-
     def __init__(self, size, point_values) -> None:
+        self.card_deck = []
 
         for i in range (1, size+1):
             card = Card(i, point_values.get(i,1))
-            Deck.CardDeck.append(card)
+            self.card_deck.append(card)
