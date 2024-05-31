5a9c242248f7ee7e79c9aadb48b6b2692e59dd7f
Vinay Gahlawat <vinay.gahlawat@gmail.com>
2024-05-31 14:25:20 -0500

    Update test to work again.

--- tests/test_dealer.py
+++ tests/test_dealer.py
@@ -7,7 +7,7 @@ class TestDealer(unittest.TestCase):
         dealer = Dealer(2, 10, 104, 4, {7:11,9:3})
         self.assertEqual(dealer.deck_size, 104)
         self.assertEqual(dealer.board_size, 4)
-        self.assertEqual(dealer.rounds_per_game, 1)
+        self.assertEqual(dealer.rounds_per_game, 10)
         self.assertEqual(len(dealer.players), 2)
         self.assertIsNotNone(dealer.players['0'].hand)
         self.assertIsNotNone(dealer.players['1'].hand)
